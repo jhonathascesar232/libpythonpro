6c911776b3e3da997b8ff08cd9c42f4d6430e51a
Jhonathas César <jhonathascesar232@gmail.com>
2021-02-02 21:30:35 -0300

    Adicionado parametrização com testes

--- libpythonprooo/tests/tests_spam/test_enviador_de_email.py
+++ libpythonprooo/tests/tests_spam/test_enviador_de_email.py
@@ -1,5 +1,6 @@
 # para testes com venv tem que importar o caminho completo
 from libpythonprooo.spam.enviador_de_email import Enviador
+import pytest
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
@@ -14,3 +15,23 @@ def test_rementente():
         'Mensagem',                         #   Mensagem
     )
     assert 'jhonathascesar232@gmail.com' in resposta
+
+
+# parametrização: utilizado para varios testes na mesma função, com listas
+# @pytest.mark.parametrize(
+#     'remetente',                                        #   VÁRIAVEL A SER UTILIZADA NA FUNÇÃO
+#     ['jhonathascesar232@gmail.com', 'bol@gmail.com']    #   Lista que vai ser iterada
+# )
+
+@pytest.mark.parametrize(
+    'remetente',['jhonathascesar232@gmail.com', 'bol@gmail.com']
+)
+def test_rementente_com_parametrize(remetente):                         # VARIAVEL RECEBIDA DO DECORATOR
+    enviador = Enviador()
+    resposta = enviador.enviar(
+        remetente,                          #   de
+        'silva@gmail.com',                  #   para
+        'Assunto',                          #   Assunto
+        'Mensagem',                         #   Mensagem
+    )
+    assert remetente in resposta
