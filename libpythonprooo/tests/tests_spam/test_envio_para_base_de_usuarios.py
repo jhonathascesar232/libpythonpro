2a34ff009ffba3aeb38b62bbfa4df910f7e602b2
Jhonathas César <jhonathascesar232@gmail.com>
2021-02-07 18:10:56 -0300

    Produção de codigo testavel

--- libpythonprooo/tests/tests_spam/test_envio_para_base_de_usuarios.py
+++ libpythonprooo/tests/tests_spam/test_envio_para_base_de_usuarios.py
@@ -2,9 +2,8 @@ from libpythonprooo.spam.enviador_de_email import Enviador
 from libpythonprooo.spam.main import EnviadorDeSpam
 
 
-# Iniciando com conftest.py > compartilhar as fixture o com o
-# pacote correspondente a que pertence
-def test_envio_de_spam(sessao):
+# Produção de codigo testavel
+def test_qtd_de_spam(sessao):
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails(
         remetente='renzo@python.pro.br',
