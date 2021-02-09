57507300f31785616f3ae745187abffeaba775cd
Jhonathas César <jhonathascesar232@gmail.com>
2021-02-09 18:34:53 -0300

    Adicionado pytest-mock com a fixture mocker no test_gitgub_api

--- libpythonprooo/tests/tests_spam/test_github_api.py
+++ libpythonprooo/tests/tests_spam/test_github_api.py
@@ -5,18 +5,8 @@ import pytest
 from libpythonprooo import github_api
 
 
-# Módulo como Objeto: Iniciando
-# Teste buscar avatar
-# 1 Isolando testes, 2 - contruir o Mock(Substitui as funçõe do módulo original)
-
-def test_buscar_avatar(avatar_url):
-    # Faz uma chamada normal com as alterações
-    url = github_api.buscar_avatar('renzo')
-    assert url == avatar_url
-
-
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):  # Utiliza a fixture mocker do pytest-mock
     resp_mock = Mock()  # Emula o response
     avatar = 'https://avatars.githubusercontent.com/u/402714?v=4'
     resp_mock.json.return_value = {  # Emula o respone.json()
@@ -24,14 +14,21 @@ def avatar_url():
         'id': 402714,
         'avatar_url': avatar,
     }
-    # Setup
-    # get original
-    get_original = github_api.requests.get
+    # Setup     # endereco completo
+    get_mock = mocker.patch('libpythonprooo.github_api.requests.get')
     # Alterado o Objeto requests de github_api para o Teste Unitario
-    github_api.requests.get = Mock(return_value=resp_mock)  # Atribui o resp_mock ao requests.get
-    yield avatar
-    # Tear Down
-    github_api.requests.get = get_original
+    get_mock.return_value = resp_mock  # Atribui o resp_mock ao requests.get
+    return avatar
+
+
+# Módulo como Objeto: Iniciando
+# Teste buscar avatar
+# 1 Isolando testes, 2 - contruir o Mock(Substitui as funçõe do módulo original)
+
+def test_buscar_avatar(avatar_url):
+    # Faz uma chamada normal com as alterações
+    url = github_api.buscar_avatar('renzon')
+    assert url == avatar_url
 
 
 # Isolamento de Imports / Teste de integração que faz busca na api do github
@@ -39,5 +36,5 @@ def test_buscar_avatar_integrao():
     url = github_api.buscar_avatar('renzo')
     assert 'https://avatars.githubusercontent.com/u/402714?v=4' == url
 
-
 # Bibliotéca Pytest-mock
+# o mock faz a funcao de restaurar o lib de volta ao original
